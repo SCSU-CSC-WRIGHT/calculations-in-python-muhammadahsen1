9a55f43834c952d88fa3d9f0846b94899228bfe6
muhammadahsen1 <160660355+muhammadahsen1@users.noreply.github.com>
2024-09-25 15:16:46 -0400

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,4 +1,4 @@
 kg = int(input("what is your weight?"))
-m = int(input("what is your heaight?"))
-BMI = (22.86) # you are in a healthy BMI range
-print(f"the weight and height is: {BMI}")
+height = float(input("what is your heaight?"))
+BMI = kg/height**2 # you are in a healthy BMI range
+print(f"You BMI is: {round(BMI)}")
