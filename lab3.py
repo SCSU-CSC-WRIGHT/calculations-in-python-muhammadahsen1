3c8383f559408b1ba29be75d2875a8bb51430837
muhammadahsen1 <160660355+muhammadahsen1@users.noreply.github.com>
2024-09-25 15:06:46 -0400

    Create lab3.py

---
+++ lab3.py
@@ -0,0 +1,4 @@
+celsius = int(input("temptrature in celsius")
+fahrenheit = int(input("from celsius to fahrenheit")
+answer = 77.0 # this is answer of temprature from celsius to fahrenheit
+print(f"temprature from cesius to fahrenheit is: {answer}")
